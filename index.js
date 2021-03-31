ba21073c7884222b2d2a07275f8df55807fc2b5b
francesta <huntleyfrancesta@gmail.com>
2021-03-30 23:06:26 -0400

    commit tree c4fc91c2453f72d24b06e82398692bf1466a80f6

--- index.js
+++ index.js
@@ -1,11 +1,38 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+let distance;
+let city;
+let tip;
+
+function scuberGreetingForFeet(distance) {
+    if (distance <= 400) {
+        return "This one is on me!";
+    }
+    if (distance > 2000 && distance <= 2500) {
+        return "I will gladly take your thirty bucks.";
+
+    }
+    if (distance > 2500) {
+        return 'No can do.';
+    }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city) {
+    // Write your code here!
+    let message = city === "NYC" ? "Ok, sounds good." : "No go.";
+    return message;
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip) {
+    // Write your code here!
+    switch (tip) {
+        case 'generous':
+            return "Thank you so much.";
+            break;
+
+        case 'not as generous':
+            return "Thank you.";
+            break;
+        default:
+            return "Bye.";
+    }
+
 }
